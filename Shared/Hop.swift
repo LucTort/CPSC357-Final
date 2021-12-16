b7c986d4fc2f996d6c63922b71014f00e495dfdf
Andrew Dillon <adillon@chapman.edu>
2021-12-15 21:09:06 -0800

    Formatted Hop.swift

--- Shared/Hop.swift
+++ Shared/Hop.swift
@@ -8,13 +8,14 @@
 //This specifies the types of data contained in each item
 
 import SwiftUI
-struct Hop : Codable, Identifiable {
+struct Hop : Codable, Identifiable
+{
     var id: String//required forname identifiable
+    
     var name: String
     var volume: Float
     var weight: Float
     var alphaAcidContent: Float
-
     var boilTime: Int
     var imageName: String
 }
